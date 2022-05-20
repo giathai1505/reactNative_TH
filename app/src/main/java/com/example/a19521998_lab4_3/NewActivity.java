e3c7639478b7e71a697eea7c6a0a9474637461bd
Ngô Gia Thái <81607294+Thai-Sieu-Cap-VIP-Pro@users.noreply.github.com>
2022-05-20 12:44:08 +0700

    Thai

---
+++ app/src/main/java/com/example/a19521998_lab4_3/NewActivity.java
@@ -0,0 +1,30 @@
+package com.example.a19521998_lab4_3;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class NewActivity extends AppCompatActivity {
+
+    private Button btnBack;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.new_activity);
+
+        btnBack = (Button) findViewById(R.id.btn_back);
+
+        btnBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(NewActivity.this, MainActivity.class);
+                startActivity(intent);
+                overridePendingTransition(R.anim.anim_slide_in_right, R.anim.anim_slide_out_left);
+            }
+        });
+    }
+}
